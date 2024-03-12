4bdd93264d54c63ad3452eabea0fb0fd0e6f4295
Juno <79557712+JUNUbermensch@users.noreply.github.com>
2024-03-12 15:57:25 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import streamlit as st
 import torch
 import torch.quantization
 import torch.nn.utils.prune as prune
+from torch.cuda.amp import autocast
+from transformers import T5ForConditionalGeneration
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -10,34 +12,25 @@ def fix_state_dict(state_dict):
     fixed_state_dict = {key.replace('module.', ''): value for key, value in state_dict.items()}
     return fixed_state_dict
 
-@st.cache(allow_output_mutation=True)
+@st.cache_resource
 def load_model():
-    model_name_or_path = "./t5_mss_small_torch"
-    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+    tokenizer = AutoTokenizer.from_pretrained('t5-small')
+    model = T5ForConditionalGeneration.from_pretrained('t5-small', return_dict=True)
     checkpoint = torch.load("./checkpoint.ckpt", map_location=device)
-    fixed_state_dict = fix_state_dict(checkpoint)
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name_or_path)
-    model = torch.quantization.quantize_dynamic(
-    model, {torch.nn.Linear, torch.nn.LSTM, torch.nn.Transformer}, dtype=torch.qint8
-    )
-    model.load_state_dict(fixed_state_dict)
+    model.load_state_dict(checkpoint, strict=False)
     model.to(device)
     return tokenizer, model
 
 tokenizer, model = load_model()
-module = model.linear
-prune.l1_unstructured(module, name='weight', amount=0.3)
-prune.remove(module, 'weight')
 
 def summarize(text):
-    with torch.no_grad():
-        inputs = tokenizer.encode("summarize: " + text, return_tensors="pt", max_length=512, truncation=True)
-        inputs = inputs.to(device)
-        summary_ids = module.generate(inputs, max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
+    with torch.no_grad(), autocast(enabled=device=='cuda'):
+        inputs = tokenizer.encode("summarize: " + text, return_tensors="pt", max_length=512, truncation=True).to(device)
+        summary_ids = model.generate(inputs, max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
         summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary
 
-st.title("T5_mss_small Text Summarization App")
+st.title("T5 Text Summarization App")
 
 text = st.text_area("Text to summarize", "Paste your text here...", height=300)
 if st.button("Summarize"):
