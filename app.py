1669c185098a26368b86b96a8ddb31dbd601d21a
Juno <79557712+JUNUbermensch@users.noreply.github.com>
2024-03-12 17:18:17 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer
 import streamlit as st
 import torch
 import torch.quantization
 import torch.nn.utils.prune as prune
-from torch.cuda.amp import autocast
 from transformers import T5ForConditionalGeneration
 
 device = "cpu"
@@ -38,7 +37,7 @@ def load_model():
 tokenizer, model = load_model()
 
 def summarize(text):
-    with torch.no_grad(), autocast(enabled=device == 'cuda'):
+    with torch.no_grad():
         inputs = tokenizer.encode("summarize: " + text, return_tensors="pt", max_length=512, truncation=True).to(device)
         summary_ids = model.generate(inputs, max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
         summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
@@ -51,4 +50,4 @@ if st.button("Summarize"):
     st.subheader("Summary")
     st.write(summary)
     
-# streamlit run app.py server.port 40 --server.address 0.0.0.0
+# streamlit run app.py server.port 8501 --server.address 0.0.0.0
